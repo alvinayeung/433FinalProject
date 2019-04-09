741662e4274e2ace1bc0fd0f9fd6ade153244b99
Alvina Yeung <alvinayeung@gmail.com>
2019-04-09 15:17:12 -0400

    test 100

--- app/src/main/java/com/example/a433finalproject/DirectionActivity.java
+++ app/src/main/java/com/example/a433finalproject/DirectionActivity.java
@@ -14,6 +14,7 @@ public class DirectionActivity extends AppCompatActivity {
         System.out.print("IT WORKED OH MY GOSH");
         System.out.print("IT WORKED OH MY GOSH!");
         System.out.print("HELLLLLLOOOFOHDSFHDSFHSDJFHSDJFHSDJFHSDJH");
+        System.out.print("IT WORKED OH MY GOSH!");
     }
 
 }
