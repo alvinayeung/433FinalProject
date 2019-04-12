679aee81479a6953f9a51d2c2bf26f26c1d13fec
Alvina Yeung <alvinayeung@gmail.com>
2019-04-12 11:26:25 -0400

    fixed database syntax errors when logged databse is correct

--- app/src/main/java/com/example/a433finalproject/Question.java
+++ app/src/main/java/com/example/a433finalproject/Question.java
@@ -1,5 +1,6 @@
 package com.example.a433finalproject;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
@@ -22,47 +23,55 @@ public class Question extends AppCompatActivity {
 
         db=openOrCreateDatabase("MyDatabse", Context.MODE_PRIVATE, null);
         db.execSQL("DROP TABLE IF EXISTS GreenCompass");
-        db.execSQL("CREATE TABLE A3 (itemID int, Item text, Bins, text)");
+        db.execSQL("CREATE TABLE GreenCompass (itemID int, Item text, Bins text)");
 
-        db.execSQL("insert into GreenCompass values('100','water bottle', 'recycle');");
-        db.execSQL("insert into GreenCompass values('101','paper', 'recycle');");
-        db.execSQL("insert into GreenCompass values('102','soda Can', 'recycle');");
-        db.execSQL("insert into GreenCompass values('103','glass', 'recycle');");
-        db.execSQL("insert into GreenCompass values('104','newspaper', 'recycle')");
-        db.execSQL("insert into GreenCompass values('105','paper towels', 'recycle')");
-        db.execSQL("insert into GreenCompass values('106','napkins', 'recycle')");
-        db.execSQL("insert into GreenCompass values('107','tissues', 'recycle')");
-        db.execSQL("insert into GreenCompass values('108','grocery bags', 'recycle')");
-        db.execSQL("insert into GreenCompass values('109','straws', 'recycle')");
-        db.execSQL("insert into GreenCompass values('110','solo cups', 'recycle')");
-        db.execSQL("insert into GreenCompass values('111','newspaper', 'recycle')");
-        db.execSQL("insert into GreenCompass values('112','lenoir fork', 'recycle')");
-        db.execSQL("insert into GreenCompass values('113','lenoir spoon', 'recycle')");
-        db.execSQL("insert into GreenCompass values('114','lenoir knife', 'recycle')");
-        db.execSQL("insert into GreenCompass values('115','rams fork', 'recycle')");
-        db.execSQL("insert into GreenCompass values('116','rams spoon', 'recycle')");
-        db.execSQL("insert into GreenCompass values('117','rams knife', 'recycle')");
-        db.execSQL("insert into GreenCompass values('118','index cards', 'recycle')");
+        db.execSQL("insert into GreenCompass values(100,'water bottle', 'recycle');");
+        db.execSQL("insert into GreenCompass values(101,'paper', 'recycle');");
+        db.execSQL("insert into GreenCompass values(102,'soda can', 'recycle');");
+        db.execSQL("insert into GreenCompass values(103,'glass', 'recycle');");
+        db.execSQL("insert into GreenCompass values(104,'newspaper', 'recycle')");
+        db.execSQL("insert into GreenCompass values(105,'paper towels', 'recycle')");
+        db.execSQL("insert into GreenCompass values(106,'napkins', 'recycle')");
+        db.execSQL("insert into GreenCompass values(107,'tissues', 'recycle')");
+        db.execSQL("insert into GreenCompass values(108,'grocery bags', 'recycle')");
+        db.execSQL("insert into GreenCompass values(109,'straws', 'recycle')");
+        db.execSQL("insert into GreenCompass values(110,'solo cups', 'recycle')");
+        db.execSQL("insert into GreenCompass values(111,'newspaper', 'recycle')");
+        db.execSQL("insert into GreenCompass values(112,'index cards', 'recycle')");
 
+        db.execSQL("insert into GreenCompass values(200,'lenoir box', 'compost');");
+        db.execSQL("insert into GreenCompass values(201,'food', 'compost')");
+        db.execSQL("insert into GreenCompass values(202,'green lenoir fork', 'compost')");
+        db.execSQL("insert into GreenCompass values(203,'green lenoir spoon', 'compost')");
+        db.execSQL("insert into GreenCompass values(204,'green lenoir knife', 'compost')");
+        db.execSQL("insert into GreenCompass values(205,'green rams fork', 'compost')");
+        db.execSQL("insert into GreenCompass values(206,'green rams spoon', 'compost')");
+        db.execSQL("insert into GreenCompass values(207,'green rams knife', 'compost')");
 
-        db.execSQL("insert into GreenCompass values('200','lenoir box', 'compost');");
-        db.execSQL("insert into GreenCompass values('201','food', 'compost')");
+        db.execSQL("insert into GreenCompass values(301,'chip bag', 'trash')");
+        db.execSQL("insert into GreenCompass values(302,'plastic', 'trash')");
+        db.execSQL("insert into GreenCompass values(303,'paper towels', 'trash')");
+        db.execSQL("insert into GreenCompass values(304,'napkins', 'trash')");
+        db.execSQL("insert into GreenCompass values(305,'tissues', 'trash')");
+        db.execSQL("insert into GreenCompass values(306,'grocery bags', 'trash')");
+        db.execSQL("insert into GreenCompass values(307,'straws', 'trash')");
+        db.execSQL("insert into GreenCompass values(308,'solo cups', 'trash')");
+        db.execSQL("insert into GreenCompass values(309,'plastic spoon', 'trash')");
+        db.execSQL("insert into GreenCompass values(310,'plastic fork', 'trash')");
+        db.execSQL("insert into GreenCompass values(311,'plastic knife', 'trash')");
+        db.execSQL("insert into GreenCompass values(312,'to go coffee cups', 'trash')");
 
-        db.execSQL("insert into GreenCompass values('301','chip bag', 'trash')");
-        db.execSQL("insert into GreenCompass values('302','plastic', 'trash')");
-        db.execSQL("insert into GreenCompass values('303','paper towels', 'trash')");
-        db.execSQL("insert into GreenCompass values('304','napkins', 'trash')");
-        db.execSQL("insert into GreenCompass values('305','tissues', 'trash')");
-        db.execSQL("insert into GreenCompass values('306','grocery bags', 'trash')");
-        db.execSQL("insert into GreenCompass values('307','straws', 'trash')");
-        db.execSQL("insert into GreenCompass values('308','solo cups', 'trash')");
-        db.execSQL("insert into GreenCompass values('309','plastic spoon', 'trash')");
-        db.execSQL("insert into GreenCompass values('310','plastic fork', 'trash')");
-        db.execSQL("insert into GreenCompass values('311','plastic knife', 'trash')");
-        db.execSQL("insert into GreenCompass values('312','to go coffee cups', 'trash')");
+        db.execSQL("insert into GreenCompass values(400,'cardboard', 'trash_cardboard')");
+        db.execSQL("insert into GreenCompass values(401,'cardboard box', 'trash_cardboard')");
 
-        db.execSQL("insert into GreenCompass values('400','cardboard', 'trash_cardboard')");
-        db.execSQL("insert into GreenCompass values('401','cardboard box', 'trash_cardboard')");
+        //test
+//        Cursor c=db.rawQuery("select * from GreenCompass", null);
+//        c.moveToFirst();
+//        for(int i=0; i<c.getCount(); i++){
+//            Log.v("PRINT DATABASE", "DataBase: " +
+//                    + c.getInt(0) + ", " + c.getString(1) + ", " + c.getString(2));
+//            c.moveToNext();
+//        }
 
     }
 
@@ -70,8 +79,6 @@ public class Question extends AppCompatActivity {
 
         Intent x = new Intent(Question.this, RouteActivity.class);
         startActivity(x);
-
-
 
     }
 
