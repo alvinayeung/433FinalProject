7b6b3ec53b8cf9bc178c410f286e643548374c63
Alvina Yeung <alvinayeung@gmail.com>
2019-04-16 19:22:58 -0400

    updated style of main activity

--- app/src/main/java/com/example/a433finalproject/FoundLoc.java
+++ app/src/main/java/com/example/a433finalproject/FoundLoc.java
@@ -2,6 +2,8 @@ package com.example.a433finalproject;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,32 +30,69 @@ public class FoundLoc extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_found_loc);
 
-        TextView part1 = (TextView) findViewById(R.id.part1sent);
-        TextView part2 = (TextView) findViewById(R.id.part2sent);
-        TextView part3 = (TextView) findViewById(R.id.part3sent);
+        Bundle extras = getIntent().getExtras();
 
-        Bundle bundle= getIntent().getExtras();
-        String searchString= bundle.getString("ETstring");
+        if(extras.getByteArray("picture") != null) {
+            byte[] byteArray = extras.getByteArray("picture");
 
-        part1.setText("Your " + searchString);
+            Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+            ImageView image = (ImageView) findViewById(R.id.LoadImageID);
 
-        String part2String = bundle.getString("bin");
+            image.setImageBitmap(bmp);
 
-        if(part2String.matches("recycle")){
-            part2.setText("is recyclable");
-        }else if (part2String.matches("compost")){
-            part2.setText("is compostable");
-        }else if(part2String.matches("trash")){
-            part2.setText("is trash");
+            TextView part1 = (TextView) findViewById(R.id.part1sent);
+            TextView part2 = (TextView) findViewById(R.id.part2sent);
+            TextView part3 = (TextView) findViewById(R.id.part3sent);
+
+
+            String searchString = extras.getString("ETstring");
+
+            part1.setText("Your " + searchString);
+
+            String part2String = extras.getString("bin");
+
+            if (part2String.matches("recycle")) {
+                part2.setText("is recyclable");
+            } else if (part2String.matches("compost")) {
+                part2.setText("is compostable");
+            } else if (part2String.matches("trash")) {
+                part2.setText("is trash");
+            } else if (part2String.matches("trash_cardboard")) {
+                part2.setText("is cardboard trash");
+            }
+
+            part3.setText(" " + String.valueOf(extras.getDouble("minDistance")) + " ");
+        }else{
+
+            TextView part1 = (TextView) findViewById(R.id.part1sent);
+            TextView part2 = (TextView) findViewById(R.id.part2sent);
+            TextView part3 = (TextView) findViewById(R.id.part3sent);
+
+
+            String searchString = extras.getString("ETstring");
+
+            part1.setText(searchString);
+
+            String part2String = extras.getString("bin");
+
+            if (part2String.matches("recycle")) {
+                part2.setText("recyclable");
+            } else if (part2String.matches("compost")) {
+                part2.setText("compostable");
+            } else if (part2String.matches("trash")) {
+                part2.setText("trash");
+            } else if (part2String.matches("trash_cardboard")) {
+                part2.setText("cardboard trash");
+            }
+
+            part3.setText(" " + String.valueOf(extras.getDouble("minDistance")) + " ");
         }
-
-        part3.setText("The closest disposal for your item is " + String.valueOf(bundle.getDouble("minDistance")) + " feet away.");
-
     }
 
     public void updateNo(View view) {
 
-
+        Intent a = new Intent(FoundLoc.this, Finished.class);
+        startActivity(a);
     }
 
 
